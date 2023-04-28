b08c893f808f136aba93ddcd198e80885d126f6b
TonyVoro <tony.vorontsov@ametikool.ee>
2023-04-28 10:02:20 +0300

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const heading = document.getElementsByTagName('h1')[0];
+const catsContainer = document.getElementById('container');
+
+heading.innerText = 'Tony and cat breeds';
+
+fetch('https://catfact.ninja/breeds?limit=100')
+.then(res => res.json())
+.then(cats => {
+
+    cats.data.forEach( cat => {
+
+        const catDiv = document.createElement('div');
+
+        catDiv.classList.add('cat');
+
+        catDiv.innerHTML = `
+            <div>${cat.breed.bold()}</div>
+            <div>${cat.country}</div>
+        `;
+
+        catsContainer.append(catDiv);
+       
+    });
+});
